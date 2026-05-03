e0a469673665259909c07d4c99bbc3ecc79e7553
techmaleek <37821382-techmaleek@users.noreply.replit.com>
2026-05-03 16:11:03 +0000

    Git commit prior to merge

--- migrations/0002_classy_oracle.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "invoices" ADD COLUMN "currency" text DEFAULT 'USD' NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "currency" text DEFAULT 'USD' NOT NULL;
--- migrations/0006_bent_echo.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "ooo_requests" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;
